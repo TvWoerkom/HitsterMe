86440545a1cbc76ef51e81f8b6d280026147d658
TeunvanWoerkom <teun.vanwoerkom@deltares.nl>
2026-09-08 21:37:10 +0200

    test button

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 #%%
 from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Get the parent directory (project root)
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def hello():
@@ -9,7 +13,7 @@ def hello():
   
 @app.route("/qr_scanning") 
 def qr_coding(): 
-    return render_template('qr_coding.html') 
+    return render_template('../qr_coding.html') 
 
 if __name__ == '__main__':
   app.run()
